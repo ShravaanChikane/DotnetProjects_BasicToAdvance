f3998fd7712948bf2352a46a414d3825bb419464
Shravaan Chikane <shravanchikane04@gmail.com>
2026-02-26 02:04:01 +0530

    feat: upload basic console projects 201 to 203

---
+++ 201_Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Welcome to the Calculator!");
+Console.WriteLine("---------------------------");
+Console.Write("Enter the first number: ");
+string FirstInput = Console.ReadLine();
+double num1 = Convert.ToDouble(FirstInput);
+Console.Write("Enter the second number: ");
+string SecondInput = Console.ReadLine();
+double num2 = Convert.ToDouble(SecondInput);
+
+Console.WriteLine("Select an operation:");
+Console.WriteLine("1. Addition (+)");
+Console.WriteLine("2. Subtraction (-)");
+Console.WriteLine("3. Multiplication (*)");
+Console.WriteLine("4. Division (/)");
+Console.Write("Your Choice");
+string choice = Console.ReadLine();
+
+double result = 0;
+
+switch (choice)
+{
+    case "1":
+        result = num1 + num2;
+        Console.WriteLine($"Result: {num1} + {num2} = {result}");
+        break;
+    case "2":
+        result = num1 - num2;
+        Console.WriteLine($"Result: {num1} - {num2} = {result}");
+        break;
+    case "3":
+        result = num1 * num2;
+        Console.WriteLine($"Result: {num1} * {num2} = {result}");
+        break;
+    case "4":
+        if (num2 != 0)
+        {
+            result = num1 / num2;
+            Console.WriteLine($"Result: {num1} / {num2} = {result}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please select a valid operation.");
+        break;
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ 202_StringManipulator/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("== Project 202: String Manipulator ==");
+Console.Write("Enter a Word or a Senetence: ");
+
+string input = Console.ReadLine();
+
+Console.WriteLine("-- Manipulations --");
+System.Console.WriteLine($"Uppercase: {input.ToUpper()}");
+System.Console.WriteLine($"Lowercase: {input.ToLower()}");
+System.Console.WriteLine($"Total Characters: {input.Length}");
+
+int vowelCount = 0;
+
+string vowels = "aeiouAEIOU";
+
+foreach ( char letter in input)
+{
+    if (vowels.Contains(letter))
+    {
+        vowelCount++;
+    }
+}
+Console.WriteLine($"Total Vowels: {vowelCount}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
